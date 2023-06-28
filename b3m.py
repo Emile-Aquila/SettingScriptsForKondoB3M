489502159e181c29d071f56f1fab1bf767c21c6f
Emile-Aquila <tmk8oakan6130@gmail.com>
2023-06-28 15:37:25 +0900

    add gitignore, and refactor codes

--- b3m.py
+++ b3m.py
@@ -1,25 +1,24 @@
 import serial
 import time
 
-#baudrate = 2000000
-baudrate = 1500000
-b3m = serial.Serial('/dev/ttyUSB0', baudrate=baudrate, parity=serial.PARITY_NONE, timeout=0.5)
-target_id = 0
+baudrate = 2000000
+# baudrate = 1500000
 
-#WRITEコマンド
-#メモリーマップ(RAM)のアドレス指定でデバイスのRAM上に書き込みます。
-def B3M_Write_CMD(servo_id, TxData, Address, byte_num=1): #送信コマンドを作成
-    txCmd = [0x07+byte_num, 0x04,0x00,servo_id]
+target_id = 1
+port = "/dev/ttyUSB0"
+
+b3m = serial.Serial(port, baudrate=baudrate, parity=serial.PARITY_NONE, timeout=0.5)
+
+
+# WRITEコマンド
+# メモリーマップ(RAM)のアドレス指定でデバイスのRAM上に書き込みます。
+def B3M_Write_CMD(servo_id, TxData, Address, byte_num=1):  # 送信コマンドを作成
+    txCmd = [0x07 + byte_num, 0x04, 0x00, servo_id]
     for i in range(byte_num):
-    	txCmd.append((TxData >> (i*8)) & 0xff)
-    txCmd += [Address,0x01]
-    
-    checksum = 0
-    for i in txCmd:
-        checksum += i
-    txCmd.append(checksum & 0xff)
-    print(txCmd)
-    
+        txCmd.append((TxData >> (i * 8)) & 0xff)
+    txCmd += [Address, 0x01]
+
+    txCmd.append(sum(txCmd) & 0xff)  # リストの最後にチェックサムを挿入する
     b3m.write(txCmd)
     rxCmd = b3m.read(5)
     if len(rxCmd) == 0:
@@ -28,16 +27,12 @@ def B3M_Write_CMD(servo_id, TxData, Address, byte_num=1): #送信コマンドを
     return True
 
 
-def B3M_setPos_CMD(servo_id, pos, MoveTime): #送信コマンドを作成
-    txCmd = [0x09, 0x06,0x00, servo_id,pos & 0xff,pos >> 8 & 0xff,MoveTime & 0xff, MoveTime >> 8 & 0xff] #TIME_H
-
-    checksum = 0
-    for i in txCmd:
-        checksum += i#リストの最後にチェックサムを挿入する
-    txCmd.append(checksum & 0xff)
+def B3M_setPos_CMD(servo_id, pos, MoveTime):  # 送信コマンドを作成
+    txCmd = [0x09, 0x06, 0x00, servo_id, pos & 0xff, pos >> 8 & 0xff, MoveTime & 0xff, MoveTime >> 8 & 0xff]
+    txCmd.append(sum(txCmd) & 0xff)  # リストの最後にチェックサムを挿入する
     b3m.write(txCmd)
 
-    #サーボからの返事を受け取る
+    # サーボからの返事を受け取る
     rxCmd = b3m.read(7)
 
     if len(rxCmd) == 0:
@@ -45,16 +40,12 @@ def B3M_setPos_CMD(servo_id, pos, MoveTime): #送信コマンドを作成
     return True
 
 
-def B3M_Save_CMD(servo_id): #送信コマンドを作成
-    txCmd = [0x05, 0x02, 0x00, servo_id] 
-
-    checksum = 0
-    for i in txCmd:
-        checksum += i#リストの最後にチェックサムを挿入する
-    txCmd.append(checksum & 0xff)
+def B3M_Save_CMD(servo_id):  # 送信コマンドを作成
+    txCmd = [0x05, 0x02, 0x00, servo_id]
+    txCmd.append(sum(txCmd) & 0xff)  # リストの最後にチェックサムを挿入する
     b3m.write(txCmd)
 
-    #サーボからの返事を受け取る
+    # サーボからの返事を受け取る
     rxCmd = b3m.read(5)
 
     if len(rxCmd) == 0:
@@ -62,67 +53,68 @@ def B3M_Save_CMD(servo_id): #送信コマンドを作成
     return True
 
 
-def move_test(servo_id):
-	#B3Mサーボが動作するまでの準備
-	#B3M_Write_CMD(servo_id, TxData, Address)
+def Move_Test(servo_id):
+    # B3Mサーボが動作するまでの準備
+    # 動作モード：Free (動作モードと特性を設定するため、設定書き換え中の誤動作を防止するため脱力にしておく)
+    reData = B3M_Write_CMD(servo_id, 0x02, 0x28)
 
-	#動作モード：Free (動作モードと特性を設定するため、設定書き換え中の誤動作を防止するため脱力にしておく)
-	reData = B3M_Write_CMD(servo_id, 0x02, 0x28)
-	if not reData:
-		print("No response")
+    if not reData:
+        print("No response")
 
-	#位置制御モードに設定 (角度を指定して動作するモードです)
-	reData = B3M_Write_CMD(servo_id, 0x02, 0x28)
+    # 位置制御モードに設定 (角度を指定して動作するモードです)
+    reData = B3M_Write_CMD(servo_id, 0x02, 0x28)
 
-	#軌道生成タイプ：Even (直線補間タイプの位置制御を指定)
-	reData = B3M_Write_CMD(servo_id, 0x01, 0x29)
+    # 軌道生成タイプ：Even (直線補間タイプの位置制御を指定)
+    reData = B3M_Write_CMD(servo_id, 0x01, 0x29)
+    # 起動生成タイプ : Normal
+    # reData = B3M_Write_CMD(servo_id, 0x00, 0x29)
 
-    #ゲインプリセット：No.0 (PIDのプリセットゲインを位置制御モード用に設定)
-	reData = B3M_Write_CMD(servo_id, 0x00, 0x5C)
+    # ゲインプリセット：No.0 (PIDのプリセットゲインを位置制御モード用に設定)
+    reData = B3M_Write_CMD(servo_id, 0x00, 0x5C)
 
-    #動作モード：Normal （Freeの状態からトルクオン）
-	reData = B3M_Write_CMD(servo_id, 0x00, 0x28)
+    # 動作モード：Normal （Freeの状態からトルクオン）
+    reData = B3M_Write_CMD(servo_id, 0x00, 0x28)
 
+    # ID0、500msかけて5000(50度)の位置に移動する
+    reData = B3M_setPos_CMD(servo_id, 5000, 500)
+    time.sleep(2.0)  # サーボが到達するまで次の指示を待つ
 
-    #ID0、500msかけて5000(50度)の位置に移動する
-	reData = B3M_setPos_CMD(servo_id, 5000, 500)
-	time.sleep(0.5) #サーボが到達するまで次の指示を待つ
+    reData = B3M_setPos_CMD(servo_id, -5000, 500)
+    time.sleep(2.0)  # サーボが到達するまで次の指示を待つ
 
-	reData = B3M_setPos_CMD(servo_id, -5000, 500)
-	time.sleep(0.5) #サーボが到達するまで次の指示を待つ
-
-	reData = B3M_setPos_CMD(servo_id, 0, 500)
-	time.sleep(0.5) #サーボが到達するまで次の指示を待つ
-	
-	b3m.close()
-	
-
-def change_baudrate(servo_id, new_baudrate):
-	reData = B3M_Write_CMD(servo_id, 0x02, 0x28)
-	reData = B3M_Write_CMD(servo_id, new_baudrate, 0x01, 4)
-	if reData:
-		print("Change Baudrate {} -> {}".format(baudrate, new_baudrate))
-	else:
-		print("Change Baudrate Failed!")
-	reData = B3M_Save_CMD(servo_id)
-	if reData:
-		print("New Baudrate Saved")
-	else:
-		print("Save Failed!")
-	
+    reData = B3M_setPos_CMD(servo_id, 0, 500)
+    time.sleep(2.0)  # サーボが到達するまで次の指示を待つ
 
 
-	
-	
-	
+def B3M_Change_Baudrate(servo_id, new_baudrate):
+    reData = B3M_Write_CMD(servo_id, 0x02, 0x28)  # 動作モード: Free
+    reData = B3M_Write_CMD(servo_id, new_baudrate, 0x01, 4)
+    if reData:
+        print("Change Baudrate {} -> {}".format(baudrate, new_baudrate))
+    else:
+        print("Change Baudrate Failed!")
+    reData = B3M_Save_CMD(servo_id)
+    if reData:
+        print("New Baudrate Saved")
+    else:
+        print("Save Failed!")
 
 
-#change_baudrate(target_id, 2000000)
-move_test(target_id)
+def B3m_Change_ID(servo_id, new_id):
+    reData = B3M_Write_CMD(servo_id, 0x02, 0x28)  # 動作モード: Free
+    reData = B3M_Write_CMD(servo_id, new_id, 0x00)  # IDの設定を変更
+    if reData:
+        print("Change ID {} -> {}".format(servo_id, new_id))
+    else:
+        print("Change ID Failed!")
+    reData = B3M_Save_CMD(servo_id)
+    if reData:
+        print("New ID Saved")
+    else:
+        print("Save Failed!")
+
+
+# change_baudrate(target_id, 2000000)
+Move_Test(target_id)
 
 b3m.close()
-
-
-
-
-
